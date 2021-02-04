2354edd06c4dbb658cfd38d9d4daa858f7a70a3a
marksav85 <mark.saville@outlook.com>
2021-02-04 15:46:17 +0100

    Create new div containers for card content

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
+const card = document.querySelector('.your-active-class').innerHTML;
+const previousSection = document.querySelector('#section3');
+/*const allSections = document.getElementsByTagName('section');*/
+const newSection = document.createElement('div');
+newSection.setAttribute('class', 'newContainer');
+/*newSection.insertAdjacentHTML('afterend', card);*/
+/*previousSection.insertAdjacentHTML('afterend', newSection);*/
+/*previousSection.insertAdjacentHTML('afterend', card);*/
+previousSection.appendChild(newSection);
+
+
+
+
+
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,7 +44,7 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +59,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
